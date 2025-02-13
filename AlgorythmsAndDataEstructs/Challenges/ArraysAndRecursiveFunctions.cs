25ef9d07af6e2984ad951ff8c5eeb8f7c6dfae3d
Lucas <lvksounds@gmail.com>
2025-02-13 18:28:32 -0300

    .

--- AlgorythmsAndDataEstructs/Challenges/ArraysAndRecursiveFunctions.cs
+++ AlgorythmsAndDataEstructs/Challenges/ArraysAndRecursiveFunctions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AlgorythmsAndDataEstructs.Challenges
 {
@@ -13,14 +14,28 @@ namespace AlgorythmsAndDataEstructs.Challenges
         // somando todo oum array com recursividade ao inves de um loop
         public static int Sum(int[] intArr)
         {
-            //caso base
             if (intArr.Length == 0) return 0;
             if (intArr.Length == 1) return intArr[0];
 
-            return intArr[0] + Sum(intArr.Skip(0).ToArray());
+            return intArr[0] + Sum(intArr.Skip(1).ToArray());
             
         }
 
+        // contando o a quantidade de numeros em um array 
 
+        public static int Count(int[] arr)
+        {
+            if (arr.Length == 0) return 0;   
+            return 1 + Count(arr.Skip(1).ToArray());
+        }
+
+        // encontrar o mais numero do array 
+
+        public static int FindHigherNum(int[] arr)
+        {
+            if (arr.Length == 0) return 0;
+
+            
+        }
     }
 }
